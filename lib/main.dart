85d6055123532db7df1050b786df35b5fe460b08
Marc Sanny <olaneq@gmail.com>
2020-08-24 15:32:07 +0200

    Added shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:babymoon/ui/pages/home_page.dart';
 import 'package:babymoon/ui/pages/start_page.dart';
+import 'package:babymoon/ui/text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'ui/pages/home_page.dart';
+import 'ui/widgets/custom_app_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         accentColor: Colors.yellow[600],
@@ -18,7 +24,38 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: MainScreen()
+    );
+  }
+}
+
+
+
+class MainScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar.getAppBar('title', false),
+      body: FutureBuilder(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+
+            return HomePage();
+
+          } else if (snapshot.hasError) {
+
+            return Center(  
+              child: Text(
+                'Error while loading data: ${snapshot.error.toString()}',
+                style: TextStyles.main.copyWith(color: Colors.red),
+              ),
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      )
     );
   }
 }
---
+++ lib/ui/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'app_style.dart';
+
+class TextStyles {
+
+  static TextStyle get main => TextStyle(
+     color: AppStyle.accentColor,
+     fontSize: 13,
+     fontWeight: FontWeight.w300
+  );
+}
