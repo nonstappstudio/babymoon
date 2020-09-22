82e3ef496871a31a2131f85db6059f0ec1728978
Marc Sanny <olaneq@gmail.com>
2020-09-22 16:32:08 +0200

    Finished lullabies

--- lib/ui/pages/home/lullabies_tab.dart
+++ lib/ui/pages/home/lullabies_tab.dart
@@ -1,10 +1,9 @@
-import 'dart:async';
-
 import 'package:audioplayer/audioplayer.dart';
 import 'package:babymoon/ui/app_style.dart';
 import 'package:babymoon/ui/text_styles.dart';
 import 'package:babymoon/ui/widgets/card_layout.dart';
 import 'package:babymoon/utils/space.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LullabiesTab extends StatefulWidget {
@@ -17,10 +16,6 @@ class _LullabiesTabState extends State<LullabiesTab> {
 
   final audioPlayer = AudioPlayer();
 
-  bool get _isInitialized => audioPlayer.state == AudioPlayerState.PAUSED
-                          || audioPlayer.state == AudioPlayerState.PLAYING
-                          || audioPlayer.state == AudioPlayerState.COMPLETED;
-
   int _currentLullaby = 0;
 
   Map<String, String> get _lullabies => {
@@ -30,82 +25,43 @@ class _LullabiesTabState extends State<LullabiesTab> {
     'Rock a Bye Baby':'https://www.johnsonsbaby.co.za/sites/jbaby_menap/files/rockabyebaby.mp3'
   };
 
-  String _minutesSafeString(Duration duration) {
+  bool _isRepeat;
 
-    String twoDigits(int n) => n.toString().padLeft(2, "0");
-    String twoDigitsSeconds = twoDigits(duration.inSeconds.remainder(60));
-    return '${twoDigits(duration.inHours)}h ${twoDigitsSeconds}m';
+  Widget _leadingIcon(int index) {
+
+    final isPlay = _currentLullaby == index;
+
+      return IconButton(
+        onPressed: () {
+          if (isPlay) {
+            _currentLullaby = null;
+            _pause();
+          } else {
+            _currentLullaby = index;
+            _play();
+          }
+        },
+        icon: Icon(
+          isPlay ? Icons.pause : Icons.play_arrow, 
+          color: Colors.white
+        )
+      );
   }
 
-  Duration _position;
+  Widget _trailingIcon(index) {
+    final bool isCurrent = _currentLullaby == index;
 
-  StreamSubscription _positionSubscription;
-
-  bool _isLoop;
-
-  bool _isLoopOne;
-  
-
-  Widget get _playerController => CardLayout(
-    insidePadding: 16.0,
-    color: AppStyle.backgroundColor.withOpacity(0.7),
-    child: Row(
-      children: [
-        // IconButton(
-        //   onPressed: () {
-        //     audioPlayer.state == AudioPlayerState.PLAYING
-        //       ? _pause()
-        //       : _play();
-        //   },
-        //   icon: Builder(
-        //     builder: (c) {
-
-        //       IconData iconData;
-
-        //       if (audioPlayer.state == AudioPlayerState.PLAYING) {
-        //         iconData = Icons.pause;
-        //       } else if (audioPlayer.state == AudioPlayerState.PAUSED) {
-        //         iconData = Icons.play_arrow;
-        //       } else if (audioPlayer.state == AudioPlayerState.COMPLETED) {
-        //         iconData = Icons.play_arrow;
-        //       } else {
-        //         iconData = Icons.play_arrow;
-        //       }
-
-        //       return Icon(iconData, color: AppStyle.accentColor, size: 48);
-
-        //     },
-        //   )
-        // ),
-        if (_isInitialized) Flexible(
-          flex: 4,
-          child: Slider(
-            value: _position.inSeconds.toDouble(), 
-            activeColor: AppStyle.accentColor,
-            inactiveColor: AppStyle.backgroundColor,
-            onChanged: (value) => _seekTo(value),
-            max: audioPlayer.duration.inSeconds.toDouble(),
-            min: 0,
-          ),
-        )
-      ],
-    ),
-  );
-
-  void _initializePositionSubscription() {
-    _positionSubscription = audioPlayer.onAudioPositionChanged.listen((p) {
-      setState(() {
-        _position = p;
-        if (p.inSeconds >= audioPlayer.duration.inSeconds) {
-          if (_isLoop ) {
-            _currentLullaby += 1;
-            _play();
-          } else if (_isLoop) {
-            
-          } 
-        }
-      });
-    });
+    if (isCurrent) {
+      return IconButton(
+        onPressed: () => setState(() => _isRepeat == !_isRepeat),
+        icon: Icon(
+          Icons.repeat, 
+          color: _isRepeat ? AppStyle.accentColor : AppStyle.unselectedColor
+        ),
+      );
+    } else {
+      return Container(width: 0);
+    }
   }
 
   void _play() async {
@@ -118,99 +74,63 @@ class _LullabiesTabState extends State<LullabiesTab> {
     setState(() {});
   }
 
-  void _stop() async {
-    await audioPlayer.stop();
-    setState(() {});
-  }
-
-  void _seekTo(double value) {
-    audioPlayer.seek(value);
-  }
-
   @override
   void dispose() {
-    _positionSubscription.cancel();
     super.dispose();
   }
 
   @override
   void initState() {
-    _isLoop = false;
-    _isLoopOne = false;
-    _initializePositionSubscription();
-    _currentLullaby = 0;
+    _isRepeat = false;
+    _currentLullaby = null;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    //TODO: it's not safe to align everything on the bottom, think of fab player controller
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Space(8.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Lullabies by Johnson's baby",
-                  style: TextStyles.main,
-                ),
-              ),
-              GridView.count(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 2,
-                crossAxisSpacing: 4.0,
-                mainAxisSpacing: 4.0,
-                children: _lullabies.keys.map((l) {
-                  final index = _lullabies.keys.toList().indexOf(l);
-
-                  return InkWell(
-                    onTap: () {
-                      _currentLullaby != index 
-                        ? audioPlayer.play(_lullabies[l])
-                        : audioPlayer.pause();
-                      
-                      setState(() => _currentLullaby = index);
-                    },
-                    child: CardLayout(
-                      insidePadding: 16,
-                      color: AppStyle.backgroundColor.withOpacity(0.7),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                              _currentLullaby != index 
-                                ? Icons.play_arrow 
-                                : Icons.pause,
-                              color: AppStyle.accentColor,
-                              size: 48,
-                            
-                          ),
-                          Text(
-                            l,
-                            style: TextStyles.main,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Space(16),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Lullabies provided by Johnson's baby",
+              style: TextStyles.main,
+            ),
           ),
-        ),
-        
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-          child: _playerController,
-        )
-      ],
+          Space(64),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: ListView.separated(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              separatorBuilder: (_,__) => Divider(
+                height: 16, 
+                color: Colors.transparent
+              ),
+              itemCount: _lullabies.keys.length,
+              itemBuilder: (context, index) {
+                
+                return CardLayout(
+                  insidePadding: 8,
+                  color: AppStyle.backgroundColor.withOpacity(0.7),
+                  child: ListTile(
+                    leading: _leadingIcon(index),
+                    title: Text(
+                      _lullabies.keys.toList()[index],
+                      style: TextStyles.mainWhite,
+                    ),
+                    //trailing: _trailingIcon(index)
+                  ),
+                );
+              }
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
